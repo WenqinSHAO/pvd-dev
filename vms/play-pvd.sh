f061a1e13c85ac3539e12747916419e3281a8b38
Di Bartolomeo Michael <m.dibartolomeo@student.ulg.ac.be>
2018-03-18 17:21:50 +0100

    Adding test topology for hackathon IETF101

--- vms/play-pvd.sh
+++ vms/play-pvd.sh
@@ -240,4 +240,241 @@ function ip_wrapper {
     fi
 }
 
+function e {
+	echo ==== $@
+	$@
+}
+
+scl_cmd_add hack_start_radvd hack_start_radvd
+function hack_start_radvd {
+	A=2001:67c:1230:
+
+	mkdir -p ./tmp
+
+	for e in big=e001 ben=f001 fettered=f101 ; do
+		pvd=$(echo $e | sed 's/=/ /g' | awk '{print $1}')
+		addr=$(echo $e | sed 's/=/ /g' | awk '{print $2}')
+
+cat > ./tmp/${pvd}.conf << EOF
+interface ve1br0 {
+	MaxRtrAdvInterval 500;
+	AdvLinkMTU 1500;
+	AdvSendAdvert on;
+
+	prefix ${A}${addr}::/64 {
+		AdvAutonomous on;
+		AdvRouterAddr off;
+	};
+
+	RDNSS 2001:4860:4860::8844 {
+		AdvRDNSSLifetime 800;
+	};
+
+	pvd ${pvd}.mpvd.io {
+		AdvPvdIdHttpExtraInfo on;
+		AdvPvdIdLegacy off;
+		AdvPvdAdvHeader on;
+		AdvPvdIdSequenceNumber 113;
+
+		DNSSL mpvd.io {
+			AdvDNSSLLifetime 800;
+		};
+
+	};
+
+};
+EOF
+
+		e sudo ip netns exec e1 $DIR_RADVD/radvd -C ./tmp/${pvd}.conf -m stderr -p ./tmp/${pvd}.pid
+	done
+}
+
+scl_cmd_add hack_stop_radvd hack_stop_radvd
+function hack_stop_radvd {
+	set +e
+	for pvd in big ben fettered; do
+		if [ -f ./tmp/${pvd}.pid ]; then
+			e sudo kill $(cat ./tmp/${pvd}.pid)
+		fi
+	done
+}
+
+scl_cmd_add hack_start hack_start
+function hack_start {
+	A=2001:67c:1230:
+
+	IF_UP1=enx00e08f009c4c
+	IF_UP2=enx00e08f009c41
+	IF_DOWN=enx00e1000001a8
+
+	for ns in u1 u2 r1 r2 e1 h1 h2 br0; do
+		echo "Creating namespace $ns"
+		e sudo ip netns add $ns
+		e sudo ip netns exec $ns   ip link set lo up
+	done
+
+	for ns in u1 u2 r1 r2 e1; do
+		echo "Enable routing in $ns"
+		e sudo ip netns exec $ns   sysctl -w net.ipv6.conf.all.accept_ra=0
+		e sudo ip netns exec $ns   sysctl -w net.ipv6.conf.all.forwarding=1
+		e sudo ip netns exec $ns   sysctl -w net.ipv6.conf.default.accept_ra=0
+		e sudo ip netns exec $ns   sysctl -w net.ipv6.conf.default.forwarding=1
+	done
+
+
+	links="u1:r1:e0a0 u2:r1:f0a0 u2:r2:f1a0 r1:e1:e0a1 r2:e1:f1a1 e1:br0 h1:br0 h2:br0"
+
+	# Simple connexions between namespaces
+	for e in $links; do
+		i1=$(echo $e | sed 's/:/ /g' | awk '{print $1}')
+		i2=$(echo $e | sed 's/:/ /g' | awk '{print $2}')
+		a=$(echo $e | sed 's/:/ /g' | awk '{print $3}')
+		echo "Connecting $i1 and $i2"
+		e sudo ip link add v$i1$i2 type veth peer name v$i2$i1
+		e sudo ip link set v$i1$i2 netns $i1
+		e sudo ip link set v$i2$i1 netns $i2
+		e sudo ip netns exec $i1    ip link set v$i1$i2 up
+		e sudo ip netns exec $i2    ip link set v$i2$i1 up
+		if [ "$a" != "" ]; then
+			p=$A$a
+			echo "Configuring prefix $p::/64"
+			e sudo ip netns exec $i1    ip addr add $p::1/64 dev v$i1$i2
+			e sudo ip netns exec $i2    ip addr add $p::2/64 dev v$i2$i1
+		fi
+	done
+
+	e sudo ip netns exec br0  brctl addbr br0
+	e sudo ip netns exec br0  brctl addif br0 vbr0e1
+	e sudo ip netns exec br0  brctl addif br0 vbr0h1
+	e sudo ip netns exec br0  brctl addif br0 vbr0h2
+	e sudo ip netns exec br0  ip link set br0 up
+
+	e sudo ip link set $IF_UP1 netns u1
+	e sudo ip link set $IF_UP2 netns u2
+	e sudo ip link set $IF_DOWN netns e1
+
+	addr="u1=$IF_UP1=103::2 u2=$IF_UP2=103::3 e1=$IF_DOWN=e0a2::1 e1=ve1br0=e001::1 e1=ve1br0=f001::1 e1=ve1br0=f101::1"
+	echo $addr
+
+	# More specific address configuration
+	for e in $addr ; do
+		ns=$(echo $e | sed 's/=/ /g' | awk '{print $1}')
+		if=$(echo $e | sed 's/=/ /g' | awk '{print $2}')
+		addr=$(echo $e | sed 's/=/ /g' | awk '{print $3}')
+		echo "Configuring address $A$addr/64 on $if"
+		e sudo ip netns exec $ns    ip addr add $A$addr/64 dev $if
+	done
+
+	e sudo ip netns exec e1  ip -6 route add ::/0 from ${A}e000::/56 via ${A}e0a1::1
+	e sudo ip netns exec e1  ip -6 route add ::/0 from ${A}f000::/56 via ${A}e0a1::1
+	e sudo ip netns exec e1  ip -6 route add ::/0 from ${A}f100::/56 via ${A}f1a1::1
+
+	for pvd in e0 f0 f1; do
+		for nh in 2 3; do
+			e sudo ip netns exec e1  ip -6 route add ${A}${pvd}0${nh}::/64 via ${A}e0a2::${nh}
+		done
+	done
+
+	e sudo ip netns exec r1  ip -6 route add ::/0 from ${A}e000::/56 via ${A}e0a0::1
+	e sudo ip netns exec r1  ip -6 route add ::/0 from ${A}f000::/56 via ${A}f0a0::1
+
+	e sudo ip netns exec r2  ip -6 route add ::/0 via ${A}f1a0::1
+
+	e sudo ip netns exec r1  ip -6 route add ${A}e000::/56 via ${A}e0a1::2
+	e sudo ip netns exec r1  ip -6 route add ${A}f000::/56 via ${A}e0a1::2
+	e sudo ip netns exec r2  ip -6 route add ${A}f100::/56 via ${A}f1a1::2
+
+	e sudo ip netns exec r1  ip -6 route add ${A}e0a2::/64 via ${A}e0a1::2
+	e sudo ip netns exec r2  ip -6 route add ${A}e0a2::/64 via ${A}f1a1::2
+
+	e sudo ip netns exec u1  ip -6 route add ::/0 via ${A}103::1
+	e sudo ip netns exec u2  ip -6 route add ::/0 via ${A}103::1
+
+	e sudo ip netns exec u1  ip -6 route add ${A}e000::/56 via ${A}e0a0::2
+	e sudo ip netns exec u2  ip -6 route add ${A}f000::/56 via ${A}f0a0::2
+	e sudo ip netns exec u2  ip -6 route add ${A}f100::/56 via ${A}f1a0::2
+
+	e sudo ip netns exec u1    ip link set $IF_UP1 up
+	e sudo ip netns exec u2    ip link set $IF_UP2 up
+	e sudo ip netns exec e1    ip link set $IF_DOWN up
+
+	for ns in u1 u2 r1 r2 e1 h1 h2 br0; do
+		echo ======================== $ns ==========================
+		echo ========== link
+		sudo ip netns exec $ns   ip link
+		echo ========== addr
+		sudo ip netns exec $ns   ip addr
+		echo ========== route
+		sudo ip netns exec $ns   ip -6 route
+	done
+
+
+	hack_start_radvd
+}
+
+# Switch to IOL mode
+scl_cmd_add hack_iol hack_iol
+function hack_iol {
+	for e in u1 u2 e1; do
+		e sudo ip netns exec r1    ip link set vr1$e down
+		e sudo ip netns exec r1    ip link set netns 1 dev vr1$e
+		e sudo brctl addbr br$e
+		e sudo brctl addif br$e vr1$e
+		e sudo ip link set vr1$e up
+		e sudo ip link set br$e up
+	done
+}
+
+# Switch to Linux mode mode
+scl_cmd_add hack_linux hack_linux
+function hack_linux {
+	A=2001:67c:1230:
+
+	list="u1=e0a0::2 u2=f0a0::2 e1=e0a1::1"
+
+	for a in $list ; do
+		e=$(echo $a | sed 's/=/ /g' | awk '{print $1}')
+		addr=$(echo $a | sed 's/=/ /g' | awk '{print $2}')
+		e sudo ip link set vr1$e down
+		e sudo ip link set br$e down
+		e sudo brctl delif br$e vr1$e
+		e sudo brctl delbr br$e
+		e sudo ip link set netns r1 dev vr1$e
+
+		e sudo ip netns exec r1    ip link set vr1$e up
+		e sudo ip netns exec r1    ip addr add ${A}${addr}/64 dev vr1$e
+	done
+
+	e sudo ip netns exec r1    ip -6 route add ::/0 from ${A}e000::/56 via ${A}e0a0::1
+	e sudo ip netns exec r1    ip -6 route add ::/0 from ${A}f000::/56 via ${A}f0a0::1
+	e sudo ip netns exec r1    ip -6 route add ${A}e000::/56 via ${A}e0a1::2
+	e sudo ip netns exec r1    ip -6 route add ${A}f000::/56 via ${A}e0a1::2
+	e sudo ip netns exec r1    ip -6 route add ${A}e0a2::/64 via ${A}e0a1::2
+}
+
+scl_cmd_add hack_test hack_test
+function hack_test {
+	#TARGET=2001:4860:4860::8888
+	TARGET=2001:67c:1230:103::1
+	for ns in u1 u2 r2 e1 h1 h2; do
+		e sudo ip netns exec $ns    ping6 -c 1 $TARGET
+	done
+}
+
+scl_cmd_add hack_stop hack_stop
+function hack_stop {
+	set +e
+	hack_stop_radvd
+
+	for ns in u1 u2 r1 r2 e1 h1 h2 br0; do
+		e sudo ip netns del $ns
+	done
+
+	for e in u1 u2 e1; do
+		e sudo ip link set br$e down
+		e sudo brctl delbr br$e
+		e sudo ip link del vr1$e
+	done
+}
+
 scl_main $@
